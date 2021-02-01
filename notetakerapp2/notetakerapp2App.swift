43acc28a93e3253c20274b591e819552c2fd1cc2
hakwok <hakwok@s.sfusd.edu>
2021-01-31 20:23:58 -0800

    Initial Commit

---
+++ notetakerapp2/notetakerapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  notetakerapp2App.swift
+//  notetakerapp2
+//
+//  Created by Hayden Kwok on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct notetakerapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

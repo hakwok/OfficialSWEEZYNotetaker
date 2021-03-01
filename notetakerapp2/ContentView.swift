c7fcc44dbdc9bb162be94f6b8e71c59f75347673
joieng1 <joieng@s.sfusd.edu>
2021-02-28 20:24:55 -0800

    added plusButton image

--- notetakerapp2/ContentView.swift
+++ notetakerapp2/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Image("plusButton")
         Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)).ignoresSafeArea()
             Image(systemName: "calendar")
             
